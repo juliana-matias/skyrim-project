ae7cef64b7cc8561ab114d1910edafcdf2a5f121
juliana_mauzolfsanto <juliana.mauzolfsantos@dellteam.com>
2023-08-15 17:25:29 -0300

    Add a new Character with Post

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -7,16 +7,32 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace skyrim_project.Controllers
 {
-[ApiController]
-[Route("api/[controller]")]
-public class CharacterController : ControllerBase
-{
-    private static Character nord = new Character();
-
-    [HttpGet]
-    public ActionResult<Character> Get()
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
     {
-        return Ok(nord);
+        private static List<Character> characters = new List<Character> {
+        new Character(),
+        new Character { Id =1 , Name = "MissKhajiit" }
+    };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")] //our webservice knows that this string id is the same value id of the paramater in the line below
+        public ActionResult<List<Character>> GetSingle(int id) //find an chracter by their id
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id)); // returns the first character where the id of the character equals the given id
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters) ;
+        }
     }
-}
 }
