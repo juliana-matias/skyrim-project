77e89290e253554ea2d6777045588608c22f5ffa
Juliana Matias <julianamatiaselt@gmail.com>
2023-11-08 17:51:31 -0300

    Authentication repository implementation

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace skyrim_project.Data
+{
+    public class IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
