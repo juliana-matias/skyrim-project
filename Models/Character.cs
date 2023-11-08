21909d4656fbe61c59b5e5b3bdefe66705589ba6
Juliana Matias <julianamatiaselt@gmail.com>
2023-11-08 17:36:18 -0300

    first relationship

--- Models/Character.cs
+++ Models/Character.cs
@@ -7,4 +7,8 @@ public class Character
     public int Defense { get; set; } = 10;
     public int Intelligence { get; set; } = 10;
     public RpgClass Class { get; set; } = RpgClass.Nord;
+
+    //adding properties to the models to create the relationship between tables
+    //a user can have a list of characters, but the character belongs to one user
+    public User? User { get; set; }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -6,5 +6,9 @@
         public string Name { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[0];
         public byte[] PasswordSalt { get; set; } = new byte[0];
+
+        //adding properties to the models to create the relationship between tables: in this case User and Character
+        //a user can have a list of characters, but the character belongs to one user
+        public List<Character>? Characters { get; set; }
     }
 }
