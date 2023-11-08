67fe08163fdbd7c76b006cb2c8e8f23fcc55ff7d
Juliana Matias <julianamatiaselt@gmail.com>
2023-11-08 17:08:43 -0300

    adding User table to database

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,12 +4,13 @@ namespace skyrim_project.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Character> Characters => Set<Character>();
+        public DbSet<User> Users => Set<User>();
     }
 
 }
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace skyrim_project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[0];
+        public byte[] PasswordSalt { get; set; } = new byte[0];
+    }
+}
