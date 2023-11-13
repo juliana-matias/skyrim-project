a3c35a27459c3732ca7d672d0cc1f095738a3c37
Juliana Matias <julianamatiaselt@gmail.com>
2023-11-13 17:43:11 -0300

    authentication controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using skyrim_project.Dtos.User;
+
+namespace skyrim_project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+
+        public AuthController(IAuthRepository authRepo)
+        {
+
+            _authRepo = authRepo;
+
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            //calling the method of authrepo and saving the result
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+                );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
---
+++ Dtos/User/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace skyrim_project.Dtos.User
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
